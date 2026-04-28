3f3483d4a0e99d7fc6c3754cbf8cdcf9f34f99a5
panthermp07 <139424054+panthermp07@users.noreply.github.com>
2026-04-28 13:27:58 +0530

    syntex

--- plugins/commands.py
+++ plugins/commands.py
@@ -66,7 +66,7 @@ async def start(client, message):
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, get_wish()),
             reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
+            parse_mode=enums.ParseMode.HTML,
             message_effect_id=5104841245755180586
         )
         return
